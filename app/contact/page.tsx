ebbc26172f38cd3c70f7f930ad624254a3c4d801
Hadyan Abdul Aziz <hadyanabdulaziz@gmail.com>
2024-06-27 14:12:36 +0700

    add transition pages

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,10 +2,17 @@
 import Form from "@/components/form";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { FaInstagram, FaLinkedin } from "react-icons/fa";
+import { Bounce, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Contact() {
   const router = useRouter();
+  const [isEmailHovered, setIsEmailHovered] = useState<boolean>(false);
+  const [isLocationHovered, setIsLocationHovered] = useState<boolean>(false);
+  const [isPhoneHovered, setIsPhoneHovered] = useState<boolean>(false);
+
   return (
     <div className="flex min-h-screen justify-start flex-col gap-16">
       {/* header */}
@@ -21,32 +28,89 @@ export default function Contact() {
         <div className="flex flex-col gap-4">
           <h3 className="text-primary font-medium text-3xl">LET'S MAKE A CONNECTION</h3>
           <p className="text-primary font-normal text-base">I am always looking for new ways to connect with partners. Whether you have a question, a suggestion, or just want to chat, I'm here to hear from you.</p>
-          <div className="flex flex-row gap-2 border border-primary py-2 px-4 justify-center items-center rounded-full w-fit">
-            <Image
-              src="/icons/email.png"
-              alt="icon email"
-              width={20}
-              height={20}
-            />
-            <p className="text-primary">hadyanabdulaziz@gmail.com</p>
+          <div
+            className="flex flex-row gap-2 border border-primary hover:border-secondary py-2 px-4 justify-center items-center rounded-full w-fit cursor-pointer"
+            onMouseEnter={() => {setIsEmailHovered(true)}}
+            onMouseLeave={() => {setIsEmailHovered(false)}}
+            onClick={() => {
+              navigator.clipboard.writeText('hadyanabdulaziz@gmail.com').then(
+                () => {
+                  toast.success('Email Copied!', {
+                    position: "top-center",
+                    autoClose: 900,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    transition: Bounce,
+                  });
+              })}
+            }
+          >
+            {isEmailHovered ? <p className="text-secondary px-24">Copy?</p> :
+            <>
+              <Image
+                src="/icons/email.png"
+                alt="icon email"
+                width={20}
+                height={20}
+              />
+              <p className="text-primary">hadyanabdulaziz@gmail.com</p>
+            </>
+            }
           </div>
-          <div className="flex flex-row gap-2 border border-primary py-2 px-4 justify-center items-center rounded-full w-fit">
-            <Image
-              src="/icons/location.png"
-              alt="icon location"
-              width={20}
-              height={20}
-            />
-            <p className="text-primary">Sukatani, Tapos, Kota Depok</p>
+          <div
+            className="flex flex-row gap-2 border border-primary hover:border-secondary py-2 px-4 justify-center items-center rounded-full w-fit cursor-pointer"
+            onClick={() => {router.push('https://maps.app.goo.gl/TfGx8AxqhU3oZ65P8')}}
+            onMouseEnter={() => {setIsLocationHovered(true)}}
+            onMouseLeave={() => {setIsLocationHovered(false)}}
+          >
+            {
+              isLocationHovered ? <p className="text-secondary px-16">Open Maps?</p> :
+              <>
+                <Image
+                  src="/icons/location.png"
+                  alt="icon location"
+                  width={20}
+                  height={20}
+                />
+                <p className="text-primary">Sukatani, Tapos, Kota Depok</p>
+              </>
+            }
           </div>
-          <div className="flex flex-row gap-2 border border-primary py-2 px-4 justify-center items-center rounded-full w-fit">
-            <Image
-              src="/icons/phone.png"
-              alt="icon phone"
-              width={20}
-              height={20}
-            />
-            <p className="text-primary">+6285156305278</p>
+          <div
+            className="flex flex-row gap-2 border border-primary hover:border-secondary py-2 px-4 justify-center items-center rounded-full w-fit cursor-pointer"
+            onMouseEnter={() => {setIsPhoneHovered(true)}}
+            onMouseLeave={() => {setIsPhoneHovered(false)}}
+            onClick={() => {
+              navigator.clipboard.writeText('+6285156305278').then(
+                () => {
+                  toast.success('Phone Copied!', {
+                    position: "top-center",
+                    autoClose: 900,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    transition: Bounce,
+                  });
+              })}
+            }
+          >
+            {isPhoneHovered ? <p className="text-secondary px-14">Copy?</p> :
+            <>
+              <Image
+                src="/icons/phone.png"
+                alt="icon phone"
+                width={20}
+                height={20}
+              />
+              <p className="text-primary">+6285156305278</p>
+            </>}
           </div>
           <p className="text-primary font-normal text-base">Or connect with me on social media</p>
           <div className="flex flex-row gap-2">
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,23 @@
-import type { Metadata } from "next";
+'use client'
+// import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./header";
-import GlowEffect from "@/components/glow";
+// import GlowEffect from "@/components/glow";
+import { motion, AnimatePresence } from 'framer-motion';
 import { ToastContainer } from "react-toastify";
+import { usePathname } from "next/navigation";
 
-export const metadata: Metadata = {
-  title: "Portfolio | Hadyan Abdul Aziz",
-  description: "Created with Next js",
-};
+// export const metadata: Metadata = {
+//   title: "Portfolio | Hadyan Abdul Aziz",
+//   description: "Created with Next js",
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
   return (
     <html lang="en">
       <head>
@@ -22,19 +26,34 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet" />
       </head>
       <body className="relative flex bg-background flex-col">
-        {/* <div className="flex-grow">
-        </div> */}
-        <GlowEffect />
-        <ToastContainer />
-        <div className="grid grid-cols-12">
-          <div>
-            <Header />
-          </div>
-          <div className="col-span-10">
-            {children}
-          </div>
-          <div></div>
-        </div>
+            {/* <div className="flex-grow">
+            </div> */}
+            {/* <GlowEffect /> */}
+            <ToastContainer />
+            <div className="grid grid-cols-12">
+              <div>
+                <Header />
+              </div>
+              <div className="col-span-10">
+                <AnimatePresence mode="wait">
+                  <motion.div
+                    key={pathname}
+                    initial="initial"
+                    animate="animate"
+                    // exit="exit"
+                    variants={{
+                      initial: { opacity: 0, x: -200, y: 0},
+                      animate: { opacity: 1, x: 0, y: 0},
+                      // exit: { opacity: 0, x: 200, y: 0},
+                    }}
+                    transition={{ duration: 0.5 }}
+                  >
+                      {children}
+                  </motion.div>
+                </AnimatePresence>
+              </div>
+              <div></div>
+            </div>
       </body>
     </html>
   );
